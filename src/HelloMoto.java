ef7fb33820a691db2b1b408f194dbdef04103b9a
Matthew Garza <matt.garza34@gmail.com>
2016-11-11 16:35:41 -0600

    added scanners. Made a loop that calculates perimeter.

--- src/HelloMoto.java
+++ src/HelloMoto.java
@@ -1,6 +1,9 @@
+
 /**
  * Created by Matt on 11/11/16.
  */
+
+import java.util.Scanner;
 public class HelloMoto {
 
     public static void main(String[] args) {//psvm is shortcut
@@ -20,9 +23,26 @@ public class HelloMoto {
 
         System.out.print("\n" + "this is byte " + ThisIsByte + "\n" +"this is float " + price + "\n" + "this is boolean " + sup
                 + "This is short " + ThisIsShort + "\n" + "This is int " + day + "\n" + "This is long " + ThisIsLong + "\n"
-                   + "This is double " + ThisIsDouble + "\n" + "This is char " + ThisIsChar);
+                   + "This is double " + ThisIsDouble + "\n" + "This is char " + ThisIsChar + "\n");
+
+        Scanner ThisIsScanner = new Scanner(System.in);
+        System.out.println("Enter name pls: ");
+        String name = ThisIsScanner.next();
+        System.out.println("Name is :" + name);
+
+        char choice;
+        do {
+            Scanner toContinue = new Scanner(System.in);
+            Scanner numberScanner = new Scanner(System.in);
+            System.out.println("Enter width: ");
+            double width = numberScanner.nextDouble();
+            System.out.println("Enter Length: ");
+            double length = numberScanner.nextDouble();
+            System.out.println("Perimeter is " + ((width*2) + (length*2))+ "\n" + "Continue? y/n");
+            choice = toContinue.next().charAt(0);
 
 
+        } while (choice == 'y');
 
     }
 }
