3d3bb290003de6ec0dd557ff38199b981c1507e3
Matthew Garza <matt.garza34@gmail.com>
2016-11-11 14:45:15 -0600

    Primitive data types

--- src/HelloMoto.java
+++ src/HelloMoto.java
@@ -2,8 +2,27 @@
  * Created by Matt on 11/11/16.
  */
 public class HelloMoto {
-    public static void main(String[] args) {
 
-        System.out.print("Hello Moto!!");
+    public static void main(String[] args) {//psvm is shortcut
+
+        System.out.print("Hello Moto!!");//sout is shortcut
+        //Two ways to declare variables/types
+        byte ThisIsByte = 124;
+        short ThisIsShort = 12532;
+        int day = 5;
+        //type of primitive always has to be declared
+        float price;
+        price = (float) 3.4;
+        boolean sup = true;
+        long ThisIsLong = 678765486;
+        double ThisIsDouble = 982389327;
+        char ThisIsChar = 'a';
+
+        System.out.print("\n" + "this is byte " + ThisIsByte + "\n" +"this is float " + price + "\n" + "this is boolean " + sup
+                + "This is short " + ThisIsShort + "\n" + "This is int " + day + "\n" + "This is long " + ThisIsLong + "\n"
+                   + "This is double " + ThisIsDouble + "\n" + "This is char " + ThisIsChar);
+
+
+
     }
 }
