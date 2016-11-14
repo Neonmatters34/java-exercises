4b22da14d16f9874e02c9e8830bd803a33a40008
Matthew Garza <matt.garza34@gmail.com>
2016-11-14 13:50:01 -0600

    exercise going over strings

---
+++ src/strings.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Matt on 11/14/16.
+ */
+public class strings {
+    public static void main(String[] args) {
+        String message = "WE  DON'T NEED NO EDUCATION \n WE DON'T NEED NO THOUGHT CONTROL!";
+        System.out.println(message);
+        message = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(message);
+        message = "In windows, the main drive is usually C:\\";
+        System.out.println(message);
+        message = "I can do back slashes \\, and double back slashes \\\\, and the amazing triple back-slash \\\\\\!";
+        System.out.println(message);
+        System.out.println();
+    }
+
+}
